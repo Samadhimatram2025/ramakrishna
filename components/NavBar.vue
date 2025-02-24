0d2ec19e024c157912b67c0bd77301efdb65e871
Samadhimatram2025 <jlvidyashala@gmail.com>
2025-02-24 08:55:38 +0200

    Update NavBar.vue

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -2,7 +2,7 @@
     <nav class="fixed top-0 left-0 w-full bg-white shadow-md p-4 z-10">
       <div class="max-w-6xl mx-auto flex flex-wrap justify-between items-center">
         <h2 class="text-2xl font-semibold text-saffron-800">
-          Ramakrishna Centre
+          JHB Ramakrishna Centre
         </h2>
         <ul class="flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-6 mt-4 md:mt-0">
           <li><NuxtLink to="/" class="text-gray-700 hover:text-saffron-800" active-class="text-saffron-800 font-bold">Home</NuxtLink></li>
