25f609e2a9a7b640feed64340c92abc9bbbfabc3
Samadhimatram2025 <jlvidyashala@gmail.com>
2025-03-02 21:36:19 +0200

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -165,7 +165,7 @@ export default {
       </div>
 
       <!-- Sun Animation -->
-<p class="quote text-xs text-gray-700 max-w-3xl mt-8 text-center opacity-0 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2" style="font-weight: normal;">
+<p class="quote text-xs text-gray-700 max-w-3xl mt-8 text-center opacity-0 absolute top-1/4 left-1/2 transform -translate-x-1/2 -translate-y-1/2" style="font-weight: normal;">
         The sun can give heat and light to the whole world, but he cannot do so when the clouds shut out his rays. Similarly as long as egotism veils the heart, God cannot shine upon it.
       </p>
       <div class="sun absolute bottom-0 left-1/2 transform -translate-x-1/2 w-24 h-24 rounded-full bg-gradient-to-r from-yellow-400 to-orange-500 opacity-50 z-0">
@@ -334,11 +334,7 @@ export default {
   z-index: 10;
   border-radius: 50%;
   border: 2px solid;
-  width: 40px;
-  height: 40px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
+  width: 40px; height: 40px; display: flex; align-items: center; justify-content: center;
 }
 
 .scroll-sign {
@@ -353,14 +349,14 @@ export default {
 
 @media (max-width: 768px) {
   .quote {
-    opacity: 0; /* Hide the quote on smaller screens */
-    top: 10%; /* Adjust the top position for smaller screens */
-    font-size: 0.8rem; /* Reduce font size for smaller screens */
+    opacity: 0;
+    top: 50%;
+    font-size: 0.8rem;
   }
 
   .sun {
-    bottom: 10% !important; /* Raise the sun on smaller screens */
-    transform: translateX(-50%) scale(0.7); /* Reduce the scale for smaller screens */
+    bottom: 10% !important;
+    transform: translateX(-50%) scale(0.7);
   }
 }
 </style>
